3d75ec94e921e8a6940069440ffe36f12d41605e
Vishal Vivek <vishal.vivek.10039@gmail.com>
2025-11-23 12:18:22 +0530

    ui changes and added all the functionalities

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+let slider= document.querySelector("#slider");
 let num =16;
+
+
+slider.addEventListener("input", (event)=>{
+    num = Math.sqrt(event.target.value);
+    num=Math.round(num);
+    newgrid();
+});
+
+function togglemode(){
+ if(randomcolours==false){
+    randomcolours=true;
+ }  
+ else{
+    randomcolours=false;
+ }
+}
+
 
 let box =document.querySelector(".box");
 let container = document.querySelector(".container");
 let isdrawing=false;
+let randomcolours = false;
+
 container.addEventListener("mousedown", (e)=>{
     e.preventDefault();
     isdrawing=true;
@@ -14,22 +34,60 @@ box.addEventListener("mouseup",()=>{
 
 box.addEventListener("mouseleave", ()=>{
     isdrawing=false;
-})
+});
 
-for(let i=0;i<num*num;i++){
-    let box = document.createElement("div");
-    box.style.border="solid black 1px";
-    box.style.height=600/num +'px';
-    box.style.width=600/num +'px';
-    box.style.draggable="false";    
-    box.addEventListener("mousedown",(event)=>{
-        event.target.style.backgroundColor="Yellow";
-    })
-    box.addEventListener("mouseover",(event)=>{
-        if(isdrawing){
-            box.style.backgroundColor="yellow";
-        }
-    })
-
-    container.appendChild(box);
+function getrandom(){
+    let temp = Math.random();
+    temp=Math.round(temp*255);
+    return temp;
 }
+
+
+
+function creategrid(){
+    for(let i=0;i<num*num;i++){
+        let box = document.createElement("div");
+        box.style.border="solid black 1px";
+        box.style.height=600/num +'px';
+        box.style.width=600/num +'px';
+        box.style.draggable="false";    
+        box.dataset.opacity=0;
+        box.addEventListener("mousedown",(event)=>{
+            event.target.dataset.opacity=Math.min(parseFloat(event.target.dataset.opacity)+0.1,1);
+            if(randomcolours){
+                let temp1 =getrandom();
+                let temp2=getrandom();
+                let temp3=getrandom();
+                event.target.style.backgroundColor=`rgba(${temp1}, ${temp2}, ${temp3},${event.target.dataset.opacity} )`;
+            }
+            else{
+                event.target.style.backgroundColor=`rgba(0, 0, 0,${event.target.dataset.opacity} )`;
+            }
+        })
+        box.addEventListener("mouseover",(event)=>{
+            if(isdrawing){
+                event.target.dataset.opacity=Math.min(parseFloat(event.target.dataset.opacity)+0.1,1);
+                if(randomcolours){
+                    let temp1 =getrandom();
+                    let temp2=getrandom();
+                    let temp3=getrandom();
+                    event.target.style.backgroundColor=`rgba(${temp1}, ${temp2}, ${temp3},${event.target.dataset.opacity} )`;
+                }
+                else{
+                    event.target.style.backgroundColor=`rgba(0, 0, 0,${event.target.dataset.opacity} )`;
+                }
+                
+            }
+        })
+        container.appendChild(box);
+    }
+}
+
+creategrid();
+
+
+function newgrid(){
+    container.innerHTML="";
+    creategrid();
+}
+
